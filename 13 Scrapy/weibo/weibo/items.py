646600babe912ae7c008a1431f57569e63102cd7
xiaomeng <xiao@email.com>
2018-07-04 22:30:38 +0800

    微博scrapy

---
+++ 13 Scrapy/weibo/weibo/items.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Field,Item
+
+
+class UserItem(Item):
+    collection = 'users'
+    id = Field()
+    name = Field()
+    avatar = Field()
+    cover = Field()
+    gender = Field()
+    descraption = Field()
+    fans_count = Field()
+    follows_count = Field()
+    weibos_count = Field()
+    verfied = Field()
+    verfied_reason = Field()
+    follows = Field()
+    fans = Field()
+    crawled_at =Field()
+
+
+class UserRelationItem(Item):
+    collection = 'users'
+    id = Field()
+    follows = Field()
+    fans = Field()
+
+
+class WeiboItem(Item):
+    collection = 'weibos'
+    id = Field()
+    attitudes_count = Field()
+    comments_count = Field()
+    reposts_count = Field()
+    picture = Field()
+    pictures = Field()
+    source = Field()
+    text = Field()
+    raw_text = Field()
+    thumbnail = Field()
+    user = Field()
+    created_at = Field()
+    crawl_at = Field()
+
+
+
